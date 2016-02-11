64f6fe1eb2d6efcd1c297b848bba9d95c5f00ad8
sam <samuel.piedagnel@googlemail.com>
2016-02-11 23:55:25 +0000

    brute collinear first commit

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by samue_000 on 11/02/2016.
  */
@@ -7,12 +12,29 @@ public class BruteCollinearPoints {
     private LineSegment[] segments;
 
     public BruteCollinearPoints(Point[] points) {
-        if (points[0].slopeTo(points[1]) == points[0].slopeTo(points[2])){
-            if (points[0].slopeTo(points[1]) == points[0].slopeTo(points[3])){
-
+        if ( points == null)
+            throw new NullPointerException();
+        List<LineSegment> list = new ArrayList<>();
+        for ( int i = 0; i < points.length ; i++) {
+            for (int j = i + 1 ; j < points. length; j ++) {
+                for (int k = j + 1 ; k < points. length; k ++) {
+                    for (int l = k + 1; l < points. length; l ++) {
+                        if (points[i] == null || points [j] == null || points[k] == null || points[l] == null)
+                            throw new IllegalArgumentException();
+                        double s1 = points[i].slopeTo(points[j]);
+                        double s2 = points[i].slopeTo(points[k]);
+                        double s3 = points[i].slopeTo(points[l]);
+                        if (s1 == s2 && s1 == s3){
+                            Point[] s = new Point[]{points[i],points[j],points[k],points[l]};
+                            Arrays.sort(s);
+                            numberOfSegments ++;
+                            list.add(new LineSegment(s[0],s[3]));
+                        }
+                    }
+                }
             }
-
         }
+        segments = list.toArray(new LineSegment[list.size()]);
     }
     public int numberOfSegments() {
         return numberOfSegments;
--- src/Point.java
+++ src/Point.java
@@ -67,7 +67,7 @@ public class Point implements Comparable<Point> {
         } else if (this.x == that.x){
             return Double.POSITIVE_INFINITY;
         } else {
-            return (that.y - this.y)/(that.x - this.x);
+            return (double)(that.y - this.y)/(that.x - this.x);
         }
     }
 
